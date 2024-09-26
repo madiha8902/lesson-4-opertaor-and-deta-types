d226643a78625d7e46b679a011ab69a06a5ca67c
madiha8902 <madiha.batool@bellfield.nsw.edu.au>
2024-09-26 19:14:02 +1000

    opertors and deta type

---
+++ main.py
@@ -0,0 +1,29 @@
+# name="Madiha"
+# print(name)
+# print(type(name))
+# number=96.05
+# print(number)
+# print(type(number))
+# status=True
+# print(type(status))
+
+# activity2
+no1=57
+no2=65
+print("addtion of two numbers is",no1+no2)
+no1=57
+no2=65
+print("subtration of two numbers is",no1-no2)
+no1=57
+no2=65
+print("divsion of two numbers is",no1/no2)
+no1=57
+no2=65
+print("times of two numbers is",no1*no2)
+no1=57
+no2=65
+print("remainder of two numbers is",no1%no2)
+no1=57
+no2=65
+print("equaltity of two numbers is",no1==no2)
+print("grater than",no1>no2)
